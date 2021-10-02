f7aee20e60091caa35e3d74440e987606510bfbe
Fatih Koc <fatihinemaili@gmail.com>
2021-10-02 08:47:56 +0300

    Last Working Save

--- config/settings.py
+++ config/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = (
 
 WEBPACK_LOADER = {
     'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'bundles/',
+        'BUNDLE_DIR_NAME': 'static/assets/',
         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json')
     }
 }
