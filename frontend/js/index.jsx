59bd386a5140e83202e216c91a5acad5fe9e3f2e
Fatih Koc <fatihinemaili@gmail.com>
2021-10-02 06:06:01 +0300

    JSX is working fine, webpack to be configured

--- frontend/js/index.jsx
+++ frontend/js/index.jsx
@@ -1,6 +1,21 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import App from './pages/App';
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './app';
+import ThemingLayout from './components/ThemingLayout';
+
+
+function App() {
+  return (
+    <ThemingLayout/>
+  );
+}
+
+export default App;
 
 ReactDOM.render(<App/>, document.getElementById('react-app'));
