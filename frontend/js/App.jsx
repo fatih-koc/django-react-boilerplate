fb05f8b7d55fd783e0128474b39e51bac4ba412e
Fatih Koç <fatihinemaili@gmail.com>
2021-12-20 04:00:31 +0300

    Icons working, Flags not working

--- frontend/js/App.jsx
+++ frontend/js/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import ThemingLayout from './components/ThemingLayout';
-import DropdownExampleSearchSelection from './components/Dropdown';
-
+import ThemingLayout from './components/ThemingLayout';
+import DropdownExampleSearchSelection from './components/DropdownExampleSearchSelection';
+import LoaderExampleLoader from './components/LoaderExampleLoader';
+import IconsDemo from './components/IconsDemo';
 
 function App() {
   return (
---
+++ frontend/js/components/IconsDemo.js
@@ -0,0 +1,124 @@
+import React from 'react';
+  
+const IconsDemo = () => {  
+    return (  
+        <div>  
+            <div className="ui blue inverted three item menu">  
+                <a className="item">Semantic UI Icons</a>  
+            </div>  
+            <div>  
+                <i className="audio description icon"></i>  
+                <i className="blind icon"></i>  
+                <i className="braille icon"></i>  
+                <i className="closed captioning icon"></i>  
+                <i className="closed captioning outline icon"></i>  
+                <i className="deaf icon"></i>  
+                <i className="low vision icon"></i>  
+                <i className="phone volume icon"></i>  
+                <i className="question circle icon"></i>  
+                <i className="question circle outline icon"></i>  
+                <i className="sign language icon"></i>  
+                <i className="tty icon"></i>  
+            </div>  
+            <div>  
+                <i className="angle double down icon"></i>  
+                <i className="angle double left icon"></i>  
+                <i className="angle double right icon"></i>  
+                <i className="angle double up icon"></i>  
+                <i className="angle down icon"></i>  
+                <i className="angle left icon"></i>  
+                <i className="angle right icon"></i>  
+                <i className="angle up icon"></i>  
+                <i className="arrow alternate circle up icon"></i>  
+                <i className="arrow alternate circle up outline icon"></i>  
+                <i className="arrow circle down icon"></i>  
+                <i className="arrow circle left icon"></i>  
+                <i className="arrow circle right icon"></i>  
+                <i className="arrow circle up icon"></i>  
+                <i className="arrow down icon"></i>  
+                <i className="arrow left icon"></i>  
+                <i className="arrow right icon"></i>  
+                <i className="arrow up icon"></i>  
+                <i className="arrows alternate icon"></i>  
+                <i className="arrows alternate horizontal icon"></i>  
+                <i className="arrows alternate vertical icon"></i>  
+                <i className="caret down icon"></i>  
+                <i className="caret left icon"></i>  
+                <i className="caret right icon"></i>  
+                <i className="caret square down icon"></i>  
+                <i className="caret square down outline icon"></i>  
+                <i className="caret square left icon"></i>  
+                <i className="caret square left outline icon"></i>  
+                <i className="caret square right icon"></i>  
+                <i className="caret square right outline icon"></i>  
+                <i className="caret square up icon"></i>  
+                <i className="caret square up outline icon"></i>  
+                <i className="caret up icon"></i>  
+                <i className="cart arrow down icon"></i>  
+                <i className="chart line icon"></i>  
+                <i className="chevron circle down icon"></i>  
+                <i className="chevron circle left icon"></i>  
+                <i className="chevron circle right icon"></i>  
+                <i className="chevron circle up icon"></i>  
+                <i className="chevron down icon"></i>  
+                <i className="chevron left icon"></i>  
+                <i className="chevron right icon"></i>  
+                <i className="chevron up icon"></i>  
+                <i className="cloud download icon"></i>  
+                <i className="cloud upload icon"></i>  
+                <i className="download icon"></i>  
+                <i className="exchange alternate icon"></i>  
+                <i className="expand arrows alternate icon"></i>  
+                <i className="external alternate icon"></i>  
+                <i className="external square alternate icon"></i>  
+                <i className="hand point down icon"></i>  
+                <i className="hand point down outline icon"></i>  
+                <i className="hand point left icon"></i>  
+                <i className="hand point left outline icon"></i>  
+                <i className="hand point right icon"></i>  
+                <i className="hand point right outline icon"></i>  
+                <i className="hand point up icon"></i>  
+                <i className="hand point up outline icon"></i>  
+                <i className="hand pointer icon"></i>  
+                <i className="hand pointer outline icon"></i>  
+                <i className="history icon"></i>  
+                <i className="level down alternate icon"></i>  
+                <i className="level up alternate icon"></i>  
+                <i className="location arrow icon"></i>  
+                <i className="long arrow alternate down icon"></i>  
+                <i className="long arrow alternate left icon"></i>  
+                <i className="long arrow alternate right icon"></i>  
+                <i className="long arrow alternate up icon"></i>  
+                <i className="mouse pointer icon"></i>  
+                <i className="play icon"></i>  
+                <i className="random icon"></i>  
+                <i className="recycle icon"></i>  
+                <i className="redo icon"></i>  
+                <i className="redo alternate icon"></i>  
+                <i className="reply icon"></i>  
+                <i className="reply all icon"></i>  
+                <i className="retweet icon"></i>  
+                <i className="share icon"></i>  
+                <i className="share square icon"></i>  
+                <i className="share square outline icon"></i>  
+                <i className="sign in alternate icon"></i>  
+                <i className="sign out alternate icon"></i>  
+                <i className="sort icon"></i>  
+                <i className="sort alphabet down icon"></i>  
+                <i className="sort alphabet up icon"></i>  
+                <i className="sort amount down icon"></i>  
+                <i className="sort amount up icon"></i>  
+                <i className="sort down icon"></i>  
+                <i className="sort numeric down icon"></i>  
+                <i className="sort numeric up icon"></i>  
+                <i className="sort up icon"></i>  
+                <i className="sync icon"></i>  
+                <i className="sync alternate icon"></i>  
+                <i className="text height icon"></i>  
+                 
+            </div>  
+        </div>  
+    )  
+}  
+  
+export default IconsDemo;
